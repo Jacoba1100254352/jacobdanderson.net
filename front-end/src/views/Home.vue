59ff04f09b42258bdecdb91577a7d5667558c88b
Jacob Anderson <jacobdanderson@gmail.com>
2021-04-04 00:16:26 -0600

    Setting up with Vue
    Signed-off-by: Jacob Anderson <jacobdanderson@gmail.com>

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-------------
+  -   Section   -
+  -------------->
+
+  <section class="home section flexbox_container">
+    <article id="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          I wrote a basic website for my Dad
+          <span id="italic">(With HTML and CSS only)</span>
+        </li>
+        <li>
+          I wrote and am writing a website for his student's startup he's
+          helping with
+        </li>
+        <li>I did an eagle project creating an orienteering course</li>
+      </ul>
+    </article>
+
+    <hr/>
+
+    <article id="experience">
+      <h2>Experience</h2>
+      <ul>
+        <li>I worked at Chick-Fil-A for a few years</li>
+        <li>I worked at Crumbl over the summer</li>
+        <li>I worked for my dad writing websites over the summer</li>
+        <li>I worked for my dad's student's startup over the summer</li>
+      </ul>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  created() {
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
