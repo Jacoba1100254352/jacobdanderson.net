b23d5744e6c228c3bd94562d50615d4a074d676a
Jacob Anderson <jacobdanderson@gmail.com>
2025-07-26 14:34:15 -0400

    Autogenerated file updates

--- front-end/src/components.d.ts
+++ front-end/src/components.d.ts
@@ -9,8 +9,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     EducationComponent: typeof import('./components/EducationComponent.vue')['default']
-    EventsComponent: typeof import("./components/EventsComponent.vue")["default"]
-    MapComponent: typeof import("./components/MapComponent.vue")["default"]
 	  README: typeof import("./components/README.md")["default"];
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
